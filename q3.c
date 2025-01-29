ddd77930dca597f982a9b56a73c9dbc9ae5bd6db
studysnap <studysnap123@gmail.com>
2025-01-29 23:02:24 +0530

    fix q3

--- q3.c
+++ q3.c
@@ -4,13 +4,13 @@
 int main(){
 
     int n;
-    scanf("%d\n",&n);
+    scanf("%d",&n);
     int output = 0;
 
     for(int i=1; i<=n;i++){
         char a, b, c;
 
-        scanf("%c %c %c\n",&a,&b,&c);
+        scanf("%c %c %c",&a,&b,&c);
 
         if(a=='1' && b=='1'){
             output++;
